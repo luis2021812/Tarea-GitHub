ab6cd1a480c100322a3d4ad64edaf52d6a7be10e
ejercicios-python <bran.abregoluis@gmail.com>
2022-02-25 23:23:19 -0600

    Cambio

---
+++ Entrada/Salida/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Fundamentos 
+{
+    class EntradaSalida
+    {
+        public static void Main(string[] args)
+        {
+            string nombre;
+            Console.WriteLine("Escribe tu nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre);
+            Console.Read();
+        }
+    }
+}
